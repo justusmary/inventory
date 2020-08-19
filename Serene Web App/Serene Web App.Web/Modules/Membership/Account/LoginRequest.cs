9f6e254c6adb8756205f99d33b3db0000482a68f
Justus <valzey87@gmail.com>
2020-08-19 15:39:29 +0100

    Removed spinning logo Changed login form placeholders

--- Serene Web App/Serene Web App.Web/Modules/Membership/Account/LoginRequest.cs
+++ Serene Web App/Serene Web App.Web/Modules/Membership/Account/LoginRequest.cs
@@ -8,9 +8,9 @@ namespace Serene_Web_App.Membership
     [BasedOnRow(typeof(Administration.Entities.UserRow), CheckNames = true)]
     public class LoginRequest : ServiceRequest
     {
-        [Placeholder("default username is 'admin'")]
+        [Placeholder("username")]
         public string Username { get; set; }
-        [PasswordEditor, Placeholder("default password for 'admin' is 'serenity'"), Required(true)]
+        [PasswordEditor, Placeholder("password"), Required(true)]
         public string Password { get; set; }
     }
 }
